3efefe4b834564924a6ba690de39463f9495600a
aleksey.moiseenko <aleksey.moiseenko@fogstream.ru>
2022-11-13 20:06:57 +1000

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://Alexey4717:47174717a@cluster0.nnsg0pv.mongodb.net/locallibrary?retryWrites=true&w=majority';
+const mongoose = require("mongoose");
+const dev_db_url =
+  "mongodb+srv://Alexey4717:47174717a@cluster0.nnsg0pv.mongodb.net/locallibrary?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 // const mongoDB = 'mongodb://127.0.0.1/my_database';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 const compression = require("compression");
 const helmet = require("helmet");
 
@@ -23,35 +25,35 @@ const debug = require("debug")("author");
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(compression());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 app.use(helmet());
 app.use(debug);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 app.disable("x-powered-by");
